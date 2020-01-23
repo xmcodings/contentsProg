404f96b9df6b3cd65d5d8cd414d35e36b52f4580
xmcodings <xmuniv@cau.ac.kr>
2020-01-23 16:38:15 +0900

    add resources

--- sketch.js
+++ sketch.js
@@ -1,24 +1,38 @@
 
 let div;
+var height;
+var width;
 
+let personImg;
 
 function setup()
 {
 
   var cnv = createCanvas(windowWidth-20, 500);
-
+  height = 500;
+  width = windowWidth - 20;
   cnv.style('display', 'block');
-  cnv.parent('sketch-holder');
-  background(255, 0, 200);
+  personImg = loadImage('resources/person.png');
+  //cnv.parent('sketch-holder');
+  background(255, 255, 255);
 }
 
 function draw()
 {
+
+  background(0);
+  
+  image(personImg, width/4, height/2, Width/2, height);
+
   if(mouseIsPressed)
   {
-    div = createButton('Hello ').size(100, 15);
-    div.html('World', true);
+   
+    
   }
+  //div = createButton('Hello ').size(100, 15);
+  //div.html('World', true);
+
+
 
 }
 
